06bfb4acc71476e459418be29574ab6cbdff7864
raniasabirhussain-tech <raniasabirhussain@gmail.com>
2026-02-20 21:51:48 +0500

    Create positive_negative_zero.py

---
+++ positive_negative_zero.py
@@ -0,0 +1,28 @@
+# Program Name: Positive, Negative, or Zero Checker
+# Description:
+# This program takes three numbers as input from the user.
+# It checks each number and determines whether it is positive,
+# negative, or zero using if, elif, and else statements.
+# This program helps practice conditional statements in Python.
+num1=int(input("enter the first number"))
+num2=int(input("enter the second number"))
+num3=int(input("enter the third number"))
+if num1>0:
+    print("the first number is positive ")
+elif num1<0:
+    print(" the first number is negative")
+else:
+    print("the first number is zero")
+
+if num2>0:
+     print("the second number is positive")  
+elif num2<0:
+    print("the second number is negative")
+else:
+    print("the second number is zero")
+if num3>0:
+    print("the third number is positive")
+elif num3<0:
+    print("the third number is negative")
+else:
+    print ("the third number is zero")
